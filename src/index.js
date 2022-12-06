8d011400273c7e6c13c0229b17451a21c8a7e4fe
Alfredo Alcántara <alfredo@lunadna.com>
2022-12-06 11:01:28 -0800

    Adds cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const productsRotuer = require("./routes/products");
@@ -9,6 +10,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Bootcamp!" });
