1b474abef2d3ad047a27fc9031f3cbbb8c543adb
Miguel Sanchez <ing.miguels@gmail.com>
2022-12-08 15:40:41 -0800

    Removed try catch

--- src/controller/orders-controller.js
+++ src/controller/orders-controller.js
@@ -2,17 +2,10 @@ const OrderService = require("../services/orders-service");
 
 const saveOrder = async (req, res) => {
   const order = req.body;
-  
-  try{
-    const savedOrder = await OrderService.saveOrder(order);
 
-    res.status(201).json(savedOrder);
-  }catch(error){
-    res.status(500).json({message: "Internal Error"});
-  }
-  
+  const savedOrder = await OrderService.saveOrder(order);
 
-  
+  res.status(201).json(savedOrder);
 };
 
 module.exports = {
