1b2a834e404f0c9121f8f55d711d146a6c544f4f
Miguel Sanchez <ing.miguels@gmail.com>
2022-12-08 16:01:10 -0800

    Fixed errors

--- src/controller/orders-controller.js
+++ src/controller/orders-controller.js
@@ -3,9 +3,13 @@ const OrderService = require("../services/orders-service");
 const saveOrder = async (req, res) => {
   const order = req.body;
 
-  const savedOrder = await OrderService.saveOrder(order);
+  try {
+    const savedOrder = await OrderService.saveOrder(order);
 
-  res.status(200).json(savedOrder);
+    res.status(201).json(savedOrder);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Error" });
+  }
 };
 
 module.exports = {
